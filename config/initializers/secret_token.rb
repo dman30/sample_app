bb16fe430e5dc519c6698256125043839f465145
Damoa Obiri Alcorta <damoa.alcorta@kaeuferportal.de>
2012-10-05 16:44:35 +0200

    Innitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b8e74ea99b0635b234e37f837da2e7cfaca33822f808c0126e0be58e4072c0f488f5bf39795355db2bfcf05022432eada791fa20a67222ddf08f22f8d4b08ff4'
